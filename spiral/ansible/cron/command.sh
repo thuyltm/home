fbb3e83ba5d76028f29c61a149942b08ef878488
thuy <thuyltm2201@gmail.com>
2025-11-13 09:16:57 +0700

    Add cron playbook Ansible

---
+++ spiral/ansible/cron/command.sh
@@ -0,0 +1,2 @@
+ansible-playbook cron_module_playbook.yaml
+ansible-playbook cron_module_playbook.yaml --tags remove_cron
---
+++ spiral/ansible/cron/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "This is a simple script." >> /home/label/project/cron_output.log
+date >> cron_output.log
