76a3276a90d288bb3087f9a3e6de88fa0bc6c7c0
thuy <thuyltm2201@gmail.com>
2025-12-26 15:53:06 +0700

    Fix buid image with ko

--- elliptic/serviceB/build_image.sh
+++ elliptic/serviceB/build_image.sh
@@ -2,7 +2,8 @@
 bazel run //elliptic/serviceB:load
 
 # OR another build uses ko
+#echo "Building image $IMAGE using a custom script..."
 #export KO_DOCKER_REPO=thuyltm2201
-#ko build .
+#ko build -B --sbom=none .
 echo $IMAGE
 docker push $IMAGE
