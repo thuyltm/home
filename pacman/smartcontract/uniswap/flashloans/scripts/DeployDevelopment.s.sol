f83de4841d8f01b7e3333313f7d9489c8d64abd4
thuy <thuyltm2201@gmail.com>
2026-05-11 15:34:03 +0700

    Finish source code Uniswap/Cross-Tick Swaps

---
+++ pacman/smartcontract/uniswap/flashloans/scripts/DeployDevelopment.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+
+import "../src/UniswapV3Pool.sol";
+import "../src/UniswapV3Manager.sol";
+import "../src/UniswapV3Quoter.sol";
+import "../test/ERC20Mintable.sol";
+import "../test/TestUtils.sol";
+
+contract DeployDevelopment is Script, TestUtils {
+    function run() public {
+        uint256 wethBalance = 10 ether;
+        uint256 usdcBalance = 100000 ether;
+        int24 currentTick = tick(5000);
+        uint160 currentSqrtP = sqrtP(5000);
+
+        vm.startBroadcast();
+        ERC20Mintable token0 = new ERC20Mintable("Wrapped Ether", "WETH", 18);
+        ERC20Mintable token1 = new ERC20Mintable("USD Coin", "USDC", 18);
+
+        UniswapV3Pool pool = new UniswapV3Pool(
+            address(token0),
+            address(token1),
+            currentSqrtP,
+            currentTick
+        );
+
+        UniswapV3Manager manager = new UniswapV3Manager();
+        UniswapV3Quoter quoter = new UniswapV3Quoter();
+
+        token0.mint(msg.sender, wethBalance);
+        token1.mint(msg.sender, usdcBalance);
+
+        vm.stopBroadcast();
+
+        console.log("WETH address", address(token0));
+        console.log("USDC address", address(token1));
+        console.log("Pool address", address(pool));
+        console.log("Manager address", address(manager));
+        console.log("Quoter address", address(quoter));
+    }
+}
