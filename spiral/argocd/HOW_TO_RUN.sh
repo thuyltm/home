369e98bffcf185d0b31e00353bd531631326ee9a
thuy <thuyltm2201@gmail.com>
2026-06-20 10:35:27 +0700

    Fix install app Argocd

--- spiral/argocd/HOW_TO_RUN.sh
+++ spiral/argocd/HOW_TO_RUN.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
+# 1. Login argocd first
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+argocd login localhost:8080 \
+  --username admin \
+  --password argocd@123 \
+  --insecure
 argocd cluster add minikube --in-cluster
 argocd cluster list
 #SERVER                          NAME      VERSION  STATUS      MESSAGE  PROJECT
 #https://kubernetes.default.svc  minikube  v1.35.1  Successful
+# 2. Install
 argocd app create guestbook \
   --repo https://github.com/thuyltm/home.git \
-  --path spiral/argocd \
+  --path spiral/argocd/guestbook \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace default \
   --sync-policy automated \
